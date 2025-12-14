a652212eefa3a34f1cedfdcf176cee7ce283e3c4
hubl3r <33307877+hubl3r@users.noreply.github.com>
2025-12-13 22:40:59 -0900

    Reverting json and next config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,4 @@
-// next.config.js
 /** @type {import('next').NextConfig} */
-const withMDX = require('@next/mdx')({
-  extension: /\.mdx$/,
-});
-const nextConfig = withMDX({
-  pageExtensions: ['ts', 'tsx', 'mdx'],
-});
-module.exports = nextConfig;
+const nextConfig = {}
+
+module.exports = nextConfig
