f6c27f3da5b517975a6850374bd4f5c83e3d4159
hubl3r <33307877+hubl3r@users.noreply.github.com>
2026-01-03 18:12:24 -0900

    Create routes.ts

---
+++ app/api/test/users/routes.ts
@@ -0,0 +1,17 @@
+import { NextResponse } from 'next/server';
+import { prisma } from '@/lib/prisma';
+
+export async function GET() {
+  const users = await prisma.user.findMany({
+    select: {
+      id: true,
+      clerkId: true,
+      firstName: true,
+      lastName: true,
+      email: true
+    },
+    take: 5
+  });
+  
+  return NextResponse.json({ users });
+}
